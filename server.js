1519d42ca2763b7dcf87ad3db45c5d632b579b1b
umairayub79 <umairayub79@mgmail.com>
2023-08-11 11:45:42 +0500

    origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://victorious-lion-clothes.cyclic.cloud/",
+    origin: "https://mflixi.netlify.app/",
     credentials: "true",
   })
 );
